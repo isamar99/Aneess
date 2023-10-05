dcebcfc145f183f4f964218e9f9c3eecfe256a34
isamar99 <samarahmad@gmail.com>
2023-10-05 12:05:15 +0300

    Initial Commit

---
+++ Aneess/AneessApp.swift
@@ -0,0 +1,17 @@
+//
+//  AneessApp.swift
+//  Aneess
+//
+//  Created by Samar AlDasman on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct AneessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aneess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aneess
+//
+//  Created by Samar AlDasman on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
